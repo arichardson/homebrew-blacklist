cc40fbe4d7a1a5dc711eb31f927633454537725f
Alexander Richardson <arichardson.kde@gmail.com>
2018-07-10 09:47:59 +0100

    Add URL to the package
    It's just an empty tarball but it seems to be required by homebrew

--- Formula/blacklist-python@2.rb
+++ Formula/blacklist-python@2.rb
@@ -1,6 +1,7 @@
 class BlacklistPythonAT2 < Formula
   desc "Prevent installation of python@2 formula"
   version "2.7"
+  url "https://github.com/arichardson/homebrew-blacklist/releases/download/0.0.1/empty.tar"
 
   conflicts_with "python@2", :because => "This is the sole purpose of this formula"
 
