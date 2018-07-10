441b654cec5b8cb7e5f69cf35da6e92f21401e7e
Alexander Richardson <arichardson.kde@gmail.com>
2018-07-10 09:51:06 +0100

    Update blacklist-python@2.rb

--- Formula/blacklist-python@2.rb
+++ Formula/blacklist-python@2.rb
@@ -2,11 +2,16 @@ class BlacklistPythonAT2 < Formula
   desc "Prevent installation of python@2 formula"
   version "2.7"
   url "https://github.com/arichardson/homebrew-blacklist/releases/download/0.0.1/empty.tar"
+  sha256 "5f70bf18a086007016e948b04aed3b82103a36bea41755b6cddfaf10ace3c6ef"
 
   conflicts_with "python@2", :because => "This is the sole purpose of this formula"
+  keg_only "Because this is just a blacklist"
 
   def install
     system "echo", "Blacklisting python@2"
+    # need at least one file otherwise homebrew complains:
+    system "mkdir", "#{share}"
+    system "touch", "#{share}/info"
   end
 
   test do
