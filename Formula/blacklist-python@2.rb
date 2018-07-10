ab2d1dccd91f7aac70a087abc0aca695c9670fec
Alex Richardson <arichardson.kde@gmail.com>
2018-07-10 09:39:39 +0100

    Initial attempt at a python@2 blacklist formula

---
+++ Formula/blacklist-python@2.rb
@@ -0,0 +1,16 @@
+class BlacklistPythonAT2 < Formula
+  desc "Prevent installation of python@2 formula"
+  homepage "none"
+  url "/dev/null"
+  version "2.7"
+
+  conflicts_with "python@2", :because => "This is the sole purpose of this formula"
+
+  def install
+    system "echo", "Blacklisting python@2"
+  end
+
+  test do
+    system "false"
+  end
+end
