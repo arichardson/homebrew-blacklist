fca69d3ea4ba5436dccbb21367af8f53e45440ad
Alexander Richardson <arichardson.kde@gmail.com>
2018-07-10 09:42:45 +0100

    Remove url

--- Formula/blacklist-python@2.rb
+++ Formula/blacklist-python@2.rb
@@ -1,7 +1,5 @@
 class BlacklistPythonAT2 < Formula
   desc "Prevent installation of python@2 formula"
-  homepage "none"
-  url "/dev/null"
   version "2.7"
 
   conflicts_with "python@2", :because => "This is the sole purpose of this formula"
